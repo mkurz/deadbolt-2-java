85e310b12e14218a3d7ddac227e72ff8c671dee3
Steve Chaloner <github-NOSPAM@objectify.be>
2012-12-11 21:35:50 +0100

    moved deadbolt-java to its own repo

---
+++ app/be/objectify/deadbolt/java/actions/RestrictionsAction.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2012 Steve Chaloner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.objectify.deadbolt.java.actions;
+
+import be.objectify.deadbolt.core.models.Subject;
+import be.objectify.deadbolt.java.DeadboltHandler;
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Implements the {@link Restrictions} functionality, i.e. within an {@link And} roles are ANDed, and between
+ * {@link And} the role groups are ORed.
+ *
+ * @author Steve Chaloner (steve@objectify.be)
+ */
+public class RestrictionsAction extends AbstractRestrictiveAction<Restrictions>
+{
+    public RestrictionsAction()
+    {
+        // no-op
+    }
+
+    public RestrictionsAction(Restrictions configuration,
+                              Action<?> delegate)
+    {
+        this.configuration = configuration;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public Result applyRestriction(Http.Context ctx,
+                                   DeadboltHandler deadboltHandler) throws Throwable
+    {
+        Result result;
+
+        if (isAllowed(ctx,
+                      deadboltHandler))
+        {
+            markActionAsAuthorised(ctx);
+            result = delegate.call(ctx);
+        }
+        else
+        {
+            markActionAsUnauthorised(ctx);
+            result = onAccessFailure(deadboltHandler,
+                                     configuration.content(),
+                                     ctx);
+        }
+
+        return result;
+    }
+
+
+    private boolean isAllowed(Http.Context ctx,
+                              DeadboltHandler deadboltHandler)
+    {
+        Subject subject = getSubject(ctx,
+                                     deadboltHandler);
+
+        boolean roleOk = false;
+        if (subject != null)
+        {
+            List<String[]> roleGroups = getRoleGroups();
+
+            for (int i = 0; !roleOk && i < roleGroups.size(); i++)
+            {
+                roleOk = checkRole(subject,
+                                   roleGroups.get(i));
+            }
+        }
+
+        return roleOk;
+    }
+
+    public List<String[]> getRoleGroups()
+    {
+        List<String[]> roleGroups = new ArrayList<String[]>();
+        for (And group : configuration.value())
+        {
+            roleGroups.add(group.value());
+        }
+        return roleGroups;
+    }
+
+    @Override
+    public Class<? extends DeadboltHandler> getDeadboltHandlerClass()
+    {
+        return configuration.handler();
+    }
+}
---
+++ app/be/objectify/deadbolt/java/actions/SubjectNotPresentAction.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2012 Steve Chaloner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.objectify.deadbolt.java.actions;
+
+import be.objectify.deadbolt.core.models.Subject;
+import be.objectify.deadbolt.java.DeadboltHandler;
+import be.objectify.deadbolt.java.utils.RequestUtils;
+import play.mvc.Http;
+import play.mvc.Result;
+
+/**
+ * Implements the {@link SubjectNotPresent} functionality, i.e. the
+ * {@link be.objectify.deadbolt.core.models.Subject} provided by the {@link be.objectify.deadbolt.java.DeadboltHandler}
+ * must be null to have access to the resource.
+ *
+ * @author Steve Chaloner (steve@objectify.be)
+ */
+public class SubjectNotPresentAction extends AbstractDeadboltAction<SubjectNotPresent>
+{
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Result execute(Http.Context ctx) throws Throwable
+    {
+        Result result;
+        if (isActionUnauthorised(ctx))
+        {
+            result = onAccessFailure(getDeadboltHandler(configuration.handler()),
+                                     configuration.content(),
+                                     ctx);
+        }
+        else
+        {
+            DeadboltHandler deadboltHandler = getDeadboltHandler(configuration.handler());
+            Subject subject = getSubject(ctx,
+                                         deadboltHandler);
+
+            if (subject == null)
+            {
+                markActionAsAuthorised(ctx);
+                result = delegate.call(ctx);
+            }
+            else
+            {
+                markActionAsUnauthorised(ctx);
+                result = onAccessFailure(deadboltHandler,
+                                         configuration.content(),
+                                         ctx);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Gets the {@link be.objectify.deadbolt.core.models.Subject} from the {@link DeadboltHandler}, and logs an error if it's not present. Note that
+     * at least one actions ({@link Unrestricted} does not not require a Subject to be present.
+     *
+     * @param ctx             the request context
+     * @param deadboltHandler the Deadbolt handler
+     * @return the Subject, if any
+     */
+    @Override
+    protected Subject getSubject(Http.Context ctx,
+                                 DeadboltHandler deadboltHandler)
+    {
+        return RequestUtils.getSubject(deadboltHandler,
+                                       ctx);
+    }
+}
