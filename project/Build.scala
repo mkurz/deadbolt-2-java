d68df04315fefe0a1ddd441071cfc01686b66751
schaloner <bitbucket@objectify.be>
2014-01-31 16:37:19 +0100

    Updated correct version number

--- project/Build.scala
+++ project/Build.scala
@@ -5,12 +5,12 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "deadbolt-java"
-  val appVersion      = "2.2-RC4"
+  val appVersion      = "2.2-RC5"
 
   val appDependencies = Seq(
     javaCore,
     cache,
-    "be.objectify" %% "deadbolt-core" % "2.2-RC5"
+    "be.objectify" %% "deadbolt-core" % "2.2-RC4"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
