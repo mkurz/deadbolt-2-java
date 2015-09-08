1d02a292723e9e602969e3fc33328fc2ac3e5ad6
Steve Chaloner <schaloner@users.noreply.github.com>
2015-09-08 09:01:51 +0200

    Update InvertedRegexControllerConstraintsTest.java

--- test-app/test/be/objectify/deadbolt/java/test/controllers/pattern/InvertedRegexControllerConstraintsTest.java
+++ test-app/test/be/objectify/deadbolt/java/test/controllers/pattern/InvertedRegexControllerConstraintsTest.java
@@ -31,7 +31,7 @@ public class InvertedRegexControllerConstraintsTest
                     RestAssured.given()
                                .cookie("user", "greet")
                                .expect()
-                               .statusCode(200)
+                               .statusCode(401)
                                .when()
                                .get("/pattern/invert/regex/c/checkMatch");
                 });
