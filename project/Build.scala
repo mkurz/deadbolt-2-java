081df76c37b874dd07d3bcf8b533240d36ae0642
Steve Chaloner <bitbucket@objectify.be>
2016-03-31 11:10:28 +0200

    Release 2.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,17 +5,15 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "deadbolt-java"
-  val appVersion      = "2.2.1-SNAPSHOT"
+  val appVersion      = "2.2.0"
 
   val appDependencies = Seq(
     javaCore,
     cache,
-    "be.objectify" %% "deadbolt-core" % "2.2-RC4"
+    "be.objectify" %% "deadbolt-core" % "2.2.0"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    organization := "be.objectify",
-    resolvers += Resolver.url("Objectify Play Repository", url("http://schaloner.github.io/releases/"))(Resolver.ivyStylePatterns),
-    resolvers += Resolver.url("Objectify Play Snapshot Repository", url("http://schaloner.github.io/snapshots/"))(Resolver.ivyStylePatterns)
+    organization := "be.objectify"
   )
 }
